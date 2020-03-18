fab948c080e10fef9fab22851391b119f9a37d28
Andy <andylam1224@gmail.com>
2020-03-18 11:25:27 -0400

    working MVP buys and sellings based on SMA50

--- trading_bot.py
+++ trading_bot.py
@@ -1,18 +1,15 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import datetime as dt 
+import pandas as pd 
 from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.techindicators import TechIndicators
 import time
 import alpaca_trade_api as tradeapi
 
-
 api_key = 'J9YASUS5QZIWIKH7'
-api_key2 = '1AZ1UX26Z6TUV8T7'
-tickers = ['MSFT']
-#tickers = ['MSFT','AAPL','TSLA','AMZN','FB']
+
+tickers = ['MSFT','AAPL','TSLA','AMZN','FB']
 close_dic = {}
 averages_dic = {}
+
 api = tradeapi.REST('PKG4KMFK0CCDVQB2OSUM',
 					'C7eWpJewmvVF8a8RVWxwnrLEKRheoaEUR/ReJyI0',
 					'https://paper-api.alpaca.markets')
@@ -51,37 +48,41 @@ def sell(sym, amt):
 def get_last_price():
 
 	for symbol in tickers:
-		ts = TimeSeries(key=api_key,output_format='pandas')
-		close_price = api.get_barset(symbol,barTimeframe,limit=1)
-		close_dic[symbol] = close_price
+		close_price = api.get_barset(symbol,'1Min',limit=1)
+		close_dic[symbol] = close_price[symbol][0].c
 		print(symbol + ': ' + str(close_price))
+
 		return close_dic
 
 # get 50 day simple moving averages	using alpha vantage
 def get_sma():
 	period = 50
 
+	# Get sma and associate with ticker in a dictionary
 	for symbol in tickers:
-		ti = TechIndicators(key=api_key2, output_format='pandas') 
+		ti = TechIndicators(key=api_key, output_format='pandas') 
 		data_ti, meta_data_ti = ti.get_sma(symbol=symbol, interval='1min', time_period=period, series_type='close')
+		#Get only most recent price
 		sma = data_ti['SMA'].iloc[-1]
 		averages_dic[symbol] = sma 
+
 	return averages_dic
 
 average_dic = get_sma()
 
 def main():
 	close_dic = get_last_price()
-	print(close_dic)
+
 	for key in close_dic:
-		print(key)
+
 		# sell if close price is 5% higher than 50 day sma
 		if(close_dic[key] < averages_dic[key]*0.95):
-			print('works')
-			buy(key)
+			print('Buying at ' + str(close_dic[key]) + 'SMA50 is: ' + str(averages_dic[key]))
+			buy(key, 1)
 		elif (close_dic[key] > averages_dic[key]*1.05):
-			print('sell works')
-			sell(key)
+			print('Selling at ' + str(close_dic[key]) + 'SMA50 is: ' + str(averages_dic[key]))
+			sell(key, 1)
 	time.sleep(65)
 
-main()
+while True:
+	main()

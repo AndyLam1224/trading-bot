d610b85fd353af19e85b027cf45e9ea562989bd9
Andy <andylam1224@gmail.com>
2020-03-19 17:57:01 -0400

    updated get_tickers to check to see if historical data is up-to-date

--- get_tickers.py
+++ get_tickers.py
@@ -1,7 +1,8 @@
 import bs4 as bs
+import pandas as pd
 import requests
 from arctic import Arctic
-import time 
+from datetime import datetime
 from alpha_vantage.timeseries import TimeSeries
 
 api_key = 'J9YASUS5QZIWIKH7'
@@ -10,7 +11,7 @@ api_key = 'J9YASUS5QZIWIKH7'
 def get_tickers():
 	# Use table in wikipedia
 	resp = requests.get('https://en.wikipedia.org/wiki/S%26P_100')
-	soup = bs.BeautifulSoup(resp.text)
+	soup = bs.BeautifulSoup(resp.text, features = "html.parser")
 	table= soup.find('table',{'class':'wikitable sortable'})
 	tickers = []
 	for row in table.findAll('tr')[1:]:
@@ -33,10 +34,31 @@ symbols = get_tickers()
 # initialize timeseries
 ts = TimeSeries(key=api_key, output_format='pandas') 
 
+# Gets the date of the last day of the most recent tick data in the database
+def get_lastdate(symbol):
+	item = library.read(symbol).data
+	date = str(item.index[0])[0:10]
+	return date
+
 # Write to DB daily historical data for SP 100
-for symbol in symbols:
-	data_ts, meta_data_ts = ts.get_daily(symbol=symbol,outputsize='full')
-	df1 = data_ts
-	library.write(symbol, df1, metadata ={'source': 'AlphaVantage'})
-	time.sleep(15)
-	print(symbol + ": Complete")
+def update_tickers:
+	for symbol in symbols:
+		# If the current ticker does not exist, create new table and gather data
+		if library.has_symbol(symbol) == False:
+			print("Currently gathering data for: " + symbol) 
+			data_ts, meta_data_ts = ts.get_daily(symbol=symbol,outputsize='full')
+			df1 = data_ts
+			library.write(symbol, df1, metadata ={'source': 'AlphaVantage'})
+			time.sleep(15)
+			print(symbol + ": Complete")
+
+		# If ticker exists, check to see if its up to date, if not append newest data
+		elif(get_lastdate(symbol) != datetime.date(datetime.now())):
+			start = get_lastdate()
+			data_ts, meta_data_ts = ts.get_daily(symbol='symbol',outputsize='full')
+			new_data = data_ts[:start]
+			library.append(symbol, new_data, upsert=False)
+			print(symbol + ": Added new data")
+
+
+

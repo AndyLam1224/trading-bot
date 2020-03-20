6439e9bdf6a1b14216181828ab1af9542f34f9af
Andy <andylam1224@gmail.com>
2020-03-20 13:48:18 -0400

    Completes portfolio allocation

--- get_tickers.py
+++ get_tickers.py
@@ -3,6 +3,7 @@ import pandas as pd
 import requests
 from arctic import Arctic
 from datetime import datetime
+import datetime as dt
 from alpha_vantage.timeseries import TimeSeries
 
 api_key = 'J9YASUS5QZIWIKH7'
@@ -43,8 +44,9 @@ def get_lastdate(symbol):
 # Write to DB daily historical data for SP 100
 def update_tickers(symbols):
 	for symbol in symbols:
+
 		# If the current ticker does not exist, create new table and gather data
-		if library.has_symbol(symbol) == False:
+		if library.has_symbol(symbol) == False or get_lastdate(symbol) != datetime.date(datetime.now()):
 			print("Currently gathering data for: " + symbol) 
 			data_ts, meta_data_ts = ts.get_daily(symbol=symbol,outputsize='full')
 			df1 = data_ts
@@ -52,26 +54,32 @@ def update_tickers(symbols):
 			time.sleep(15)
 			print(symbol + ": Complete")
 
-		# If ticker exists, check to see if its up to date, if not append newest data
-		elif(get_lastdate(symbol) != datetime.date(datetime.now())):
-			start = get_lastdate(symbol)
-			data_ts, meta_data_ts = ts.get_daily(symbol=symbol, outputsize='full')
-			new_data = data_ts[:start]
-			library.append(symbol, new_data, upsert=False)
-			print(symbol + ": Added new data")
+		# Do nothing if all data up to date
+		else:
+			print("All data is up to date")
 
 # Calculate momentum of the SP100 and select the stocks with the highest 5
 def get_momentum(symbols):
+
 	momentum = {}
 	mom_list = []
 	portfolio = []
+
+	# Calculate the momentum for each stock in the db
 	for symbol in symbols:
 		item = library.read(symbol).data
 		mom = (item.iloc[0]['4. close']/item.iloc[4]['4. close'])*100
 		mom_list.append(mom)
 		momentum[mom] = symbol
 	mom_list = sorted(mom_list, reverse=True)
+
+	# adds the top 5 stocks into the list
 	for i in range(5):
 		portfolio.append(momentum[mom_list[i]])	
 	return portfolio
 
+def allocate_portfolio():
+	symbols = get_tickers()
+	#update_tickers(symbols)
+	portfolio = get_momentum(symbols)
+	return portfolio
